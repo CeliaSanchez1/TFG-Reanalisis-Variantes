cbbb6f0994dd946661aeb4757bb759e2e1332d58
Celia Sánchez Tejeda <sanchez.157177@e.unavarra.es>
2026-05-25 00:42:10 +0200

    Update separar_cromosomas.sh

--- bash/separar_cromosomas.sh
+++ bash/separar_cromosomas.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=separar_cromosomas     # Job name
-#SBATCH --output=vcf_%j.out       # Output file (%j expands to job ID)
-#SBATCH --error=vcf_%j.err        # Error file
+#SBATCH --output=separar_cromosomas_%j.out       # Output file (%j expands to job ID)
+#SBATCH --error=separar_cromosomas_%j.err        # Error file
 #SBATCH --time=2:00:00             # Walltime 
 #SBATCH --array=1-25          # Number of tasks (processes)
 #SBATCH --cpus-per-task=1           # Number of CPU cores per task
 #SBATCH --mem=4G                 # Memory per node
-#SBATCH --partition=e           # Queue/partition 
+#SBATCH --partition=           # Queue/partition 
 
-ml Miniconda3/24.7.1-0
-conda activate bcftools-env
-#Recibo como input el msVCF y utilizo la lista de cromosomas para devolver 25 msVCF, uno por cada cromosoma
+#Script para dividir el msVCF en otros msVCF a partir de la lista de regiones cromosómicas generada previamente
+
+conda activate tfg-env
 
 mkdir -p "data/cromosomas_separados"
 CHR=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$data/chromosomes.txt")
