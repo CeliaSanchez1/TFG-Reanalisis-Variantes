3ba35e029a1cd9135afb11e74362804ba74ef33e
Celia Sánchez Tejeda <sanchez.157177@e.unavarra.es>
2026-05-25 10:05:16 +0200

    Update anotacion_snpEff.py

--- python/anotacion_snpEff.py
+++ python/anotacion_snpEff.py
@@ -6,6 +6,8 @@ import gzip
 import sys
 from cyvcf2 import VCF, Writer
 
+#Script para anotar archivo VCF empleando SnpEff. Se emplea con el Wrapper correspondiente
+
 arg1 = sys.argv[1] #INPUT A ANOTAR
 arg2 = sys.argv[2] #OUTPUT ANOTADO Y COMPRIMIDO
 
@@ -13,6 +15,8 @@ def annotate_snv_SnpEff(input_vcf, output_vcf_gz, genoma_referencia):
     """
     Ejecuta SnpEff y comprime a .gz 
     """
+
+    #Ejecución de snpEff como proceso externo, capturando salida, errores y mensajes de diagnóstico
     cmd = ["snpEff", "-v", genoma_referencia, input_vcf]
     proc = subprocess.Popen(
         cmd,
@@ -20,11 +24,16 @@ def annotate_snv_SnpEff(input_vcf, output_vcf_gz, genoma_referencia):
         stderr=subprocess.PIPE,
         text=True
     )
+
+    #Comprimir el resultado en formato gz
     with gzip.open(output_vcf_gz, "wt") as out_f:
         for line in proc.stdout:
             out_f.write(line)
+            
     stderr = proc.stderr.read()
     return_code = proc.wait()
+    
+    #Captura posibles errores
     if return_code != 0:
         print("ERROR en SnpEff:")
         print(stderr)
