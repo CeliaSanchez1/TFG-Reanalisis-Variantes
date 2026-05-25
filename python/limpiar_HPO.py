775983f5847016a329940b569e8c70b61856264a
Celia Sánchez Tejeda <sanchez.157177@e.unavarra.es>
2026-05-25 10:24:17 +0200

    Update limpiar_HPO.py

--- python/limpiar_HPO.py
+++ python/limpiar_HPO.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import sys
 
-arg1 = sys.argv[7] #RUTA TABLA RELACIÓN HPO-GENES FORMATO TXT
-arg2 = sys.argv[8] #Carpeta para el tsv de salida para cada gen
-arg3 = sys.argv[9] #Carpeta para el tsv de salida reducido para cada gen
-arg4= sys.argv[10]#carpeta tsv final salida
-arg5= sys.argv[11]#carpeta tsv final salida reducido
-arg6= sys.argv[12] #Directorio de salida
+#Script para limpiar la tabla introducir la información de los HPO al TSV de variantes
+
+arg1 = sys.argv[1] #RUTA TABLA RELACIÓN HPO-GENES FORMATO TXT
+arg2 = sys.argv[2] #Carpeta para el tsv de salida para cada gen
+arg3 = sys.argv[3] #Carpeta para el tsv de salida reducido para cada gen
+arg4= sys.argv[4]#carpeta tsv final salida
+arg5= sys.argv[5]#carpeta tsv final salida reducido
+arg6= sys.argv[6] #Directorio de salida
 
 ##Introduzco la información de los HPO a la tabla de variantes modificadas
-var = pd.read_csv(arg8 sep="\t")
-var_reduced = pd.read_csv(arg9 , sep="\t")
-hpo = pd.read_csv(arg7, sep="\t")
+var = pd.read_csv(arg2 sep="\t")
+var_reduced = pd.read_csv(arg3 , sep="\t")
+hpo = pd.read_csv(arg1, sep="\t")
 
 #Elimino filas duplicadas basándome en las columnas "gene_symbol" y "hpo_id", para asegurar que cada combinación de gen y término HPO sea única
 hpo = hpo.drop_duplicates(subset=["gene_symbol", "hpo_id"])
@@ -26,8 +28,8 @@ var_reduced = var_reduced.explode("GENE_new")
 
 merged = var.merge(hpo_agg, left_on="GENE_new", right_on="gene_symbol", how="left").drop(columns=["gene_symbol"])
 merged_reduced = var_reduced.merge(hpo_agg, left_on="GENE_new", right_on="gene_symbol", how="left").drop(columns=["gene_symbol"])
-merged.to_csv(arg10, sep="\t", index=False)
-merged_reduced.to_csv(arg11, sep="\t", index=False)
+merged.to_csv(arg4, sep="\t", index=False)
+merged_reduced.to_csv(arg5, sep="\t", index=False)
 
-os.remove(arg9)
-os.remove(arg8)
+os.remove(arg3)
+os.remove(arg2)
